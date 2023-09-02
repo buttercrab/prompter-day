d3860950375d0ff0c1e904038a2e7de8d66d6eda
Jaeyong Sung <jaeyong0201@gmail.com>
2023-09-02 17:47:58 +0900

    add get

--- server/v1/session.py
+++ server/v1/session.py
@@ -4,19 +4,37 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 
 from .ai import ask
-from .db import Database, User, Session, Chat, ChatResponse
+from .db import Database, User, Session, Chat, ChatResponse, SessionResponse
 from .login import manager
 
 router = APIRouter(prefix="/session")
 
 
 @router.get("/new")
-async def new_session(name: str, user: User = Depends(manager)) -> dict:
+async def new_session(name: str, user: User = Depends(manager)) -> SessionResponse:
     async with Database.async_session() as session:
-        session.add(Session(username=user.username, session_name=name))
+        res = Session(username=user.username, session_name=name)
+        session.add(res)
         await session.commit()
 
-    return {"status": "ok"}
+    return res.to_response()
+
+
+@router.get("/get/{session_id}")
+async def get_session(
+    session_id: int, user: User = Depends(manager)
+) -> SessionResponse:
+    async with Database.async_session() as session:
+        stmt = (
+            select(Session)
+            .where(Session.username == user.username)
+            .where(Session.id == session_id)
+        )
+        res = (await session.execute(stmt)).scalar()
+        if not res:
+            raise HTTPException(404, "Session not found")
+
+        return res.to_response()
 
 
 @router.get("/list")
